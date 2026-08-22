6c8c714fcc0b2fcc683b80e72b4e7439e20f0531
alxxtexxr <alimtegar404@gmail.com>
2026-08-23 04:28:18 +0700

    Add a tool to extract bookmarked arXiv URLs from GitHub

--- src/arxiv_agent/tools/__init__.py
+++ src/arxiv_agent/tools/__init__.py
@@ -5,11 +5,13 @@ from arxiv_agent.tools.bookmarked_arxiv_paper import (
     search_bookmarked_arxiv_paper,
 )
 from arxiv_agent.tools.arxiv_paper import recommend_todays_arxiv_papers, recommend_arxiv_papers_by_date
+from arxiv_agent.tools.bookmarked_arxiv_urls_from_github import extract_bookmarked_arxiv_urls_from_github
 
 tools = [
     get_bookmarked_arxiv_papers,
     search_bookmarked_arxiv_paper,
     recommend_todays_arxiv_papers,
+    extract_bookmarked_arxiv_urls_from_github,
     recommend_arxiv_papers_by_date,
 ]
 tool_by_name = {tool.name: tool for tool in tools}
